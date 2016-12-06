a4871234ee171c76a5426ba8eb4eca70c3c2addd
223Programming <team223programming@gmail.com>
2016-12-06 15:08:27 -0500

    commit 1

--- src/Loops.java
+++ src/Loops.java
@@ -1,11 +1,32 @@
-
+/**
+ * Lesson introduces loops and modulus operator. Combines with previous knowledge of if statements, output statements, and mathematical operators.
+ * @author Leoz Maxwell Jilliumz
+ * Goal: To master loops and integrate them to simplify programming.
+ */
 public class Loops {
 
 	public static void main(String[] args)
 	{
-		/* for(Initialization; Condition; Control)
+		/* Quick Lesson: Modulus
+		   When you use the division operator, the program returns an integer (rounding) or decimal.
+		   The modulus operator (%) returns the remainder of division.
+		   Ex. 10/5 = 2 BUT 10%5 = 0. Why?
+		       9/2 = 4.5 BUT 9%2 = 1
+		       0/n = 0 AND 0%n = 0
+		       1/5 = 0.2 BUT 1%5 = 1 *Think: lower number % higher number = lower
+		   Can be used to display odd/even numbers:
+		   Odd: if(n%2 == 1)
+		   Even: if(n%2 == 0)
+		 
+		   *****************************************************************************************************************
+		   
+		   What is a loop? What does it do?
+		   
+		   For Loops
+		  
+		   for(Initialization; Condition; Control)
 		   { 
-		   		Statement(s);
+		  	 Statement(s);
 		   }
 		   
 		   Initialization: Starting Point.
@@ -19,19 +40,33 @@ public class Loops {
 		   
 		   Control: Increment/Decrement.
 		   x++ = x+1 and x-- = x-1
+		   You can use other operators too such as * and /.
 		   for(x=0;x<10;x+2)
 		   
-		   for(int x=0;x<9;x++) How many times does it run?
+		   Ex. for(int x=0;x<9;x++) How many times does it run?
 		   
-		   for(int x=1;x<=10;x++) How many times does it run?
+		   Ex. for(int x=1;x<=10;x++) How many times does it run?
 		   
-		   for(int x=10;x>0;x--)
+		   Ex. for(int x=10;x>0;x--)
+		       {
+		   		   System.out.print(x);
+		       }                            What is the output?
+		   
+		   NOW ITS TIME TO WRITE YOUR OWN! MAKE A FOR LOOP THAT DISPLAYS ONLY ODD NUMBERS (Hint: use if/else and modulus).
+		   
+		   
+		   
+		   
+		   While Loops
+		   
+		   while(condition)
 		   {
-		   		System.out.print(x);
-		   } What is the output?
+		       Statement(s);
+		       MUST INCLUDE CONTROL TO STOP!
+		   }
+		   For loops are basically condensed while loops.
 		   
-		   NOW ITS TIME TO WRITE YOUR OWN!
+		   Ex. 
 		*/
-		
 	}
 }
