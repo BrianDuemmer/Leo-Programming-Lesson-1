777588bbd1019c973bef121d44ecc2cd41f42fb6
BrianDuemmer <bduemmer1@gmail.com>
2016-12-06 12:36:43 -0500

    Initial Commit

---
+++ src/Loops.java
@@ -0,0 +1,37 @@
+
+public class Loops {
+
+	public static void main(String[] args)
+	{
+		/* for(Initialization; Condition; Control)
+		   { 
+		   		Statement(s);
+		   }
+		   
+		   Initialization: Starting Point.
+		   for(int x=# ...)
+		   OR		
+		   int x;
+		   for(x=# ...)
+		   
+		   Condition: Remember the conditional statements we reviewed last time (<, >, <=, >=, ==, !=).
+		   for(x=0;x<# ...)
+		   
+		   Control: Increment/Decrement.
+		   x++ = x+1 and x-- = x-1
+		   for(x=0;x<10;x+2)
+		   
+		   for(int x=0;x<9;x++) How many times does it run?
+		   
+		   for(int x=1;x<=10;x++) How many times does it run?
+		   
+		   for(int x=10;x>0;x--)
+		   {
+		   		System.out.print(x);
+		   } What is the output?
+		   
+		   NOW ITS TIME TO WRITE YOUR OWN!
+		*/
+		
+	}
+}
